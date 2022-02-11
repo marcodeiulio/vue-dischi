35e99bff3a8d418d5f32e3e8df433586ff4246ae
marcodeiulio <mdeiulio@icloud.com>
2022-02-11 15:13:45 +0100

    - Added AlbumCard.vue - Added dynamic generation of AlbumCard.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,72 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <main>
+      <section id="albums">
+        <div class="container">
+          <AlbumCard
+            v-for="(album, index) in albums"
+            :key="index"
+            :album-poster="album.poster"
+            :album-title="album.title"
+            :album-author="album.author"
+            :album-genre="album.genre"
+            :album-year="album.year"
+          />
+        </div>
+      </section>
+    </main>
+  </div>
 </template>
 
 <script>
+import AlbumCard from "./components/AlbumCard.vue";
+import axios from "axios";
 export default {
   name: "App",
-  components: {},
+  components: {
+    AlbumCard,
+  },
+  data() {
+    return {
+      albums: [],
+    };
+  },
+  methods: {
+    getAlbums() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((res) => {
+          this.albums = res.data.response;
+          console.log(this.albums);
+        });
+    },
+  },
+  mounted() {
+    this.getAlbums();
+  },
 };
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+img {
+  max-width: 100%;
+}
+
+#albums {
+  background-color: #1e2d3b;
+  .container {
+    width: 1080px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+}
 </style>
---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="album_card">
+    <figure>
+      <img :src="albumPoster" :alt="albumTitle" />
+      <figcaption>
+        <h3>{{ albumTitle }}</h3>
+        <p>{{ albumAuthor }}</p>
+        <p>{{ albumYear }}</p>
+      </figcaption>
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlbumCard",
+  props: [
+    "albumPoster",
+    "albumTitle",
+    "albumAuthor",
+    "albumGenre",
+    "albumYear",
+  ],
+};
+</script>
+
+<style scoped lang="scss">
+#album_card {
+  background-color: #2e3a46;
+  height: 250px;
+  width: 150px;
+  padding: 5px;
+  margin: 20px;
+  color: white;
+
+  img {
+    width: 140px;
+    height: auto;
+  }
+}
+</style>
